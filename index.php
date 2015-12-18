6b9307df82a009f3d4e24a2f244f939c646a2d1e
Kristian <kristianskog@Kristians-MacBook-Pro.local>
2015-12-18 09:39:06 +0100

    Twig
    + Added index.html
    + Twig added

--- index.php
+++ index.php
@@ -1,22 +1,2 @@
 <?php
 session_start();
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Fantastic Teaching</title>
-  </head>
-  <body>
-
-<P>Teaching</P>
-
-  </body>
-</html>
-
-<?php
-
-
-
-
-?>
