a2d5e39741be1f11f6b5299170f019242361dcc2
Klara <klarakollerstrom@gmail.com>
2015-12-18 12:30:10 +0100

    Correct index file added
    new index to correct syncing problems within group

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+session_start();
+
+$data= [
+	'title' => "Titel på sidan"
+/*'twigvar' => $object->method(),*/
+	];
+
+//Läser in Twig
+require_once 'Twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates/');
+$twig = new Twig_Environment($loader);
+echo $twig->render('index.html', $data);
+
+//Läser in klasser
+function __autoload($x) {
+	include 'classes/'.$x.'.class.php';
+}
