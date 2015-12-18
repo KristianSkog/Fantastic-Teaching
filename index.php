44d2396bc73d38b24efc61327a0e0ba05e8ce6a1
Klara <klarakollerstrom@gmail.com>
2015-12-18 10:36:59 +0100

    add files
    Inlog, connection DB, update index

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
 session_start();
+
+require_once("classes/DB.class.php");
+
+
+
+/*Laddar in Twig: */
+require_once 'Twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates/');
+$twig = new Twig_Environment($loader);
+
+/*Array med data som skickas till Twig:*/
+$data = [
+	'variabelttwignamn' => $objektnamn -> funktionsnamn(),
+	'vartwig' => $variabel,
+	];
+/*Renderar all datautskrift med Twig via index.html*/
+echo $twig -> render('index.html', $data);
+/* autoload funktion för att inkludera våra classer från separata dokument*/
+function __autoload($className) {
+	include $className.'.class.php';
+}
+?>
+
